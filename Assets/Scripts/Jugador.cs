9f68585d549ccbfa7745a72dab5083bcc87938a7
Pablo Rodriguez rodriguez <pablorodriguezrodriguez@MacBook-Air-de-Pablo.local>
2024-03-03 12:12:30 +0100

    implemtendao el saltar y el script del jugador

---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float fuerzaSalto;
+    private Rigidbody2D rigidbody2D;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Saltando",true);
+            rigidbody2D.AddForce(new Vector2(0,fuerzaSalto));
+        }
+        
+    }
+}
